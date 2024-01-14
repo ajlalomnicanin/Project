66dc098f54d98348c78c4198746f1e9340c5fc84
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2024-01-14 19:07:14 +0100

    #

--- react/src/page/Products.jsx
+++ react/src/page/Products.jsx
@@ -1,10 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Button } from "@mui/material";
 
-function Products() {
+import { fetchProducts } from "../slices/products";
+
+export const Products = () => {
+  const data = useParams();
+  const dispatch = useDispatch();
+  const productsState = useSelector((state) => state.products);
+  console.log(productsState);
+
+  const status = useSelector((state) => state.ecommerce.status);
+  const error = useSelector((state) => state.ecommerce.error);
+
+  useEffect(() => {
+    dispatch(fetchProducts(data.id));
+  }, []);
+
   return (
     <div>
-    <p>produ</p>
+      <div key={productsState.id}>
+        <img
+          className="slikaproizvoda slikaproizvoda1"
+          src={getImageUrl(
+            productsState?.product?.data?.attributes?.image.data.attributes.url
+          )}
+          alt=""
+        />
+        <p>Ime: {productsState?.product?.data?.attributes?.title}</p>
+        <p>Cena: {productsState?.product?.data?.attributes?.price}</p>
+        <p>
+          Opis proizvoda:{" "}
+          {productsState?.product?.data?.attributes?.description}
+        </p>
+        <Button variant="contained" color="secondary">
+          Add to card
+        </Button>
+      </div>
     </div>
-  )
-}
-
-export default Products
+  );
+};
